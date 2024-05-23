141574363e37bf6302bfcb6cd5c3ceea72ef1753
Depanri Purba <depanri.purba@gmail.com>
2024-05-23 11:18:08 +0700

    db update

--- application/controllers/Master.php
+++ application/controllers/Master.php
@@ -4,30 +4,36 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Master extends CI_Controller
 {
-
-	// Construct untuk memastikan ada user yang logindex()
-    public function index()
+	public function __construct()
+	{
+		parent::__construct();
+		// Jika tidak ada user yang berhasil login, kembalikan ke halaman login
+		if (!$this->session->userdata('username')) {
+			redirect('auth');
+		}
+		// Load Model
+	}
+	public function index()
 	{
 		$data['aktif'] = 'home';
 		$this->load->view('template/header');
-		$this->load->view('template/sidebar',$data);
+		$this->load->view('template/sidebar', $data);
 		$this->load->view('admin/dashboard');
 		$this->load->view('template/footer');
-	// }
+		// }
 	}
 
-    public function login()
-    {   
+	public function login()
+	{
 		$this->load->view('admin/login');
-    }
+	}
 
 	public function datagejala()
 	{
 		$data['aktif'] = 'datagejala';
 		$this->load->view('template/header');
-		$this->load->view('template/sidebar',$data);
+		$this->load->view('template/sidebar', $data);
 		$this->load->view('admin/datagejala');
 		$this->load->view('template/footer');
 	}
-
 }
---
+++ db_pakarendokrin.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 23 Bulan Mei 2024 pada 06.17
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pakarendokrin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_login`
+--
+
+CREATE TABLE `tbl_login` (
+  `id` int(11) NOT NULL DEFAULT 0,
+  `username` varchar(50) NOT NULL,
+  `password` text NOT NULL,
+  `role` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_login`
+--
+
+INSERT INTO `tbl_login` (`id`, `username`, `password`, `role`) VALUES
+(1, 'admin', '$2y$10$l6DTnLT9rK5Xjb2d8.Zu7e.uyhrl2h6YuGvZkdkOQr3o.bzMQTqd.', 'Admin');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
