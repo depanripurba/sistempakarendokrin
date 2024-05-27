5acb69cb4350db317fff5f60f190a9d35fa591d9
Nama Anda <leonardisidabutar@gmail.com>
2024-05-27 19:37:41 +0700

    update loe

--- application/views/user/hasildiagnosa.php
+++ application/views/user/hasildiagnosa.php
@@ -1,77 +1,80 @@
 <div class="content-wrapper">
-    <!-- batas -->
-    <?php
-// var_dump($hasil);
-// die;
-    ?>
     <section class="content">
-        <div class="invoice p-3 mb-3">
-            <!-- title row -->
-            <div class="row">
-                <div class="col-12">
-                    <h4>
-                        <i class="fas fa-globe"></i> Hasil Diagnosa
-                    </h4>
-                </div>
-                <!-- /.col -->
-            </div>
-            <!-- info row -->
-            <div class="row invoice-info">
-                <table>
-                    <tr>
-                        <td>Nama </td>
-                        <td>:</td>
-                        <td><?=$user['fullname']?></td>
-                    </tr>
-                    <tr>
-                        <td>Alamat </td>
-                        <td>:</td>
-                        <td>Butuh kirim alamat</td>
-                    </tr>
-                </table>
+        <div class="row">
+            <div class="col-12 mt-3">
 
+                <div class="container-fluid">
+                    <div class="invoice p-3 mb-3">
+                        <!-- title row -->
+                        <div class="row">
+                            <div class="col-12">
+                                <h4>
+                                    <i class="fas fa-notes-medical mr-2"></i>
+                                    <span>Hasil Diagnosa</span>
+                                </h4>
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <!-- info row -->
+                        <div class="row invoice-info">
+                            <div class="col-12">
+                                <div class="row mt-3">
+                                    <div class="col-1">
+                                        <span>Nama</span>
+                                    </div>
+                                    <div class="col-4"><span>: <?=$user['fullname']?></span></div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-1">
+                                        <span>Alamat</span>
+                                    </div>
+                                    <div class="col-4"><span>: [Alamat]</span></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-12">
+                                <div class="row">
+                                    <div class="col-2">Nilai Probabilitas Bayes</div>
+                                    <div class="col-1">:</div>
+                                    <div class="col-9"><?=$hasil['nilaipeluang']?> atau <?=$hasil['persenpeluang']?> %
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-2">Penyakit</div>
+                                    <div class="col-1">:</div>
+                                    <div class="col-9"><?=$hasil['kodepenyakit']?> || <?=$hasil['namapenyakit']?>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-2">Solusi</div>
+                                    <div class="col-1">:</div>
+                                    <div class="col-9"><?=$hasil['solusi']?>
+                                        <?php
+											$solusi = $hasil['solusi'];
+											$solusiArr = explode("+",$solusi);
 
-            </div>
-            <!-- /.row -->
+											print_r($solusiArr);
+										?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-            <!-- Table row -->
-            
-            <!-- /.row -->
+                        <!-- /.row -->
 
-            <div class="row">
-                <!-- accepted payments column -->
-                <div class="col-6">
-                    <p class="lead">Detail Hasil Diagnosa</p>
-
-                    <table>
-                        <tr>
-                            <td>Nilai Probabilitas Bayes</td>
-                            <td>:</td>
-                            <td><?=$hasil['nilaipeluang']?> atau <?=$hasil['persenpeluang']?> %</td>
-                        </tr>
-                        <tr>
-                            <td>Penyakit</td>
-                            <td>:</td>
-                            <td><?=$hasil['kodepenyakit']?> || <?=$hasil['namapenyakit']?></td>
-                        </tr>
-                        <tr>
-                            <td>Solusi</td>
-                            <td>:</td>
-                            <td><?=$hasil['solusi']?></td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-            
-            <!-- /.row -->
-
-            <!-- this row will not appear when printing -->
-            <div class="row no-print">
-                <div class="col-12">
-                    <a href="invoice-print.html" rel="noopener" target="_blank" class="btn btn-default"><i class="fas fa-print"></i> Print</a>
-                    <a href="<?=base_url('user/diagnosa')?>" class="btn btn-primary float-right" style="margin-right: 5px;">
-                        <i class="fas fa-back"></i> Kembali
-                    </a>
+                        <!-- this row will not appear when printing -->
+                        <div class="row no-print">
+                            <div class="col-12">
+                                <a href="invoice-print.html" rel="noopener" target="_blank" class="btn btn-default"><i
+                                        class="fas fa-print"></i> Print</a>
+                                <a href="<?=base_url('user/diagnosa')?>" class="btn btn-primary float-right"
+                                    style="margin-right: 5px;">
+                                    <i class="fas fa-back"></i> Kembali
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
