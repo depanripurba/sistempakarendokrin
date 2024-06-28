6aab4a132228a2bee402bf246efbeab93885ae34
depanripurba <depanri.purba@gmail.com>
2024-06-28 18:37:12 +0700

    remove

--- application/views/admin/datagejala.php
+++ application/views/admin/datagejala.php
@@ -23,7 +23,7 @@
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body">
-                        <table id="example2" class="table table-bordered table-hover">
+                        <table class="table table-bordered table-hover">
                             <thead>
                                 <tr>
                                     <th>Kode Gejala</th>
